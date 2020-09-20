2ec29883d29fe16c48b0f950206ea9b8abd8bd3d
lukasz <lukaszsiembida@wp.pl>
2020-09-20 13:38:58 +0200

    Strategy Template

--- src/main/java/pl/sda/wzorce/behawioralne/templatemethod/zadanie/TemplateMethodTask.java
+++ src/main/java/pl/sda/wzorce/behawioralne/templatemethod/zadanie/TemplateMethodTask.java
@@ -1,6 +1,5 @@
 package pl.sda.wzorce.behawioralne.templatemethod.zadanie;
 
-
 // napisz algorytm bazowy dla tanca ktory bedzie zawierac
 // metode dance() z wywolaniem metod:
 // stepA - system.out.pr... klasniecie
@@ -10,8 +9,67 @@ package pl.sda.wzorce.behawioralne.templatemethod.zadanie;
 // utworz klasy dziedziczace np. TaniecCzeski, TaniecPolski
 // ktore beda uzupelniac stepB i stepC, zaprezentuj dzialanie TancaPolskiego i czeskiego na ekranie
 
+abstract class Dance {
+
+    void clap() { System.out.println("Klaśnięcie"); }
+
+    protected abstract void addMove1();
+
+    void rotation() {
+        System.out.println("Obrót");
+    }
+
+    protected abstract void addMove2();
+
+    void dance() {
+
+        // klasnięcie
+       clap();
+
+        // ruch1
+
+       addMove1();
+
+        // obrót
+        rotation();
+
+        // ruch2
+       addMove2();
+    }
+}
+
+    class TaniecPolski extends Dance {
+        @Override
+        protected void addMove1() {
+            System.out.println("Piruet");
+        }
+
+        @Override
+        protected void addMove2() {
+            System.out.println("Skok");
+        }
+    }
+
+    class TaniecCzeski extends Dance {
+    @Override
+    protected void addMove1() {
+        System.out.println("Skok do tyłu");
+    }
+
+    @Override
+    protected void addMove2() {
+        System.out.println("Skok do przodu");
+    }
+}
+
 public class TemplateMethodTask {
     public static void main(String[] args) {
+        Dance taniecPolski  = new TaniecPolski();
+        Dance taniecCzeski = new TaniecCzeski();
 
+        System.out.println("Taniec Polski");
+        taniecPolski.dance();
+        System.out.println("Taniec Czeski");
+        taniecCzeski.dance();
     }
 }
--- src/main/java/pl/sda/wzorce/konstrukcyjne/prototype/zadanie/PrototypeTask.java
+++ src/main/java/pl/sda/wzorce/konstrukcyjne/prototype/zadanie/PrototypeTask.java
@@ -4,8 +4,43 @@ package pl.sda.wzorce.konstrukcyjne.prototype.zadanie;
 // zaimplementuj mechanizm klonowania
 // utworz watache psow z roznymi imionami oraz wiekiem korzystajac z
 // mechanizmu klonowania
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.lang.reflect.Type;
+
+enum Breed implements Type {
+     SHEEPDOG, BULLDOG, POODLE
+}
+
+@Data
+@AllArgsConstructor
+class Dog implements Cloneable{
+    String name;
+    Type type;
+    Integer age;
+
+    Dog createClone() throws CloneNotSupportedException {
+        return (Dog) clone();
+    }
+
+}
+
 public class PrototypeTask {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        //base
+        Dog alex = new Dog("Alex", Breed.SHEEPDOG, 5);
+
+        //prototype
+        Dog fifi = alex.createClone();
+        fifi.setName("Fifi");
+        fifi.setType(Breed.BULLDOG);
+        fifi.setAge(7);
+
+        System.out.println(alex);
+        System.out.println(fifi);
 
     }
 }
