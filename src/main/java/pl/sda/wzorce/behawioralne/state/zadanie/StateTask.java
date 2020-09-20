e906bfaf1722cf131bc7101562d8d06a0e054cf0
pjaro <pjarosz27@wp.pl>
2020-09-20 10:01:38 +0200

    Zadanie ze StateTask

--- src/main/java/pl/sda/wzorce/behawioralne/state/zadanie/StateTask.java
+++ src/main/java/pl/sda/wzorce/behawioralne/state/zadanie/StateTask.java
@@ -74,7 +74,7 @@ class PersonBahveStateDrunk implements PersonBehavState {
 
   @Override
   public void walk() {
-    System.out.println("Za mało alkoholu");
+    System.out.println("Za mało alkoholu, kto jest najbardziej trzezwy?");
   }
 
   @Override
@@ -82,38 +82,8 @@ class PersonBahveStateDrunk implements PersonBehavState {
     System.out.println("pij nie gadaj");
   }
 }
-//    private final Phone phoneStateOwner;
-//
-//    @Override
-//    public void call() {
-//        System.out.println("jestem wylaczony nie zadzwonisz");
-//    }
-//
-//    @Override
-//    public void callEnd() {
-//        System.out.println("jestem wylaczony nie skonczysz rozmowy");
-//    }
-//
-//    @Override
-//    public void sms() {
-//        System.out.println("jestem wylaczony nie smsujesz");
-//    }
-//
-//    @Override
-//    public void sendSms() {
-//        System.out.println("jestem wylaczony nie wyslesz sms");
-//    }
-//
-//    @Override
-//    public void turnOn() {
-//        phoneStateOwner.setState(PhoneState.ON);
-//        System.out.println("wlaczam sie....");
-//    }
-//
-//    @Override
-//    public void turnOff() {
-//        System.out.println("jestem juz wylaczony!");
-//    }
+
+
 
 class StateTask {
   public static void main(String[] args) {}
