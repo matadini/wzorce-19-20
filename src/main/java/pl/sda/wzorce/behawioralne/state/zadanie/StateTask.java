acd46162e3b77b5bb43c36618e6a784b663c2102
Mateusz <mateusz.iwanek94@gmail.com>
2020-09-20 09:23:05 +0200

    Zadania prostujace 2

--- src/main/java/pl/sda/wzorce/behawioralne/state/zadanie/StateTask.java
+++ src/main/java/pl/sda/wzorce/behawioralne/state/zadanie/StateTask.java
@@ -1,19 +1,15 @@
 package pl.sda.wzorce.behawioralne.state.zadanie;
 
 
-// utworz interfejs CoffeExpress reprezentujacy ekspres do kawy, ma on zawierac metody
-// wlacz, wylacz, zaparz kawe, wyczysc sie, setState - nazwy metod moga byc po angielsku
-// zaimplementuj interfejs w klasie a wewnatrz klasy obsluguj stany przy pomocy instancji interfejsu
-// CoffeExpresState, dla interfejsu utworz 4 klasy implementujace pojedyncze zachowanie
-// interfejs CoffeExpressState ma pokrywac sie swoimi metodami z interfejsem CoffeExpress
-// dla poszczegolnego stanu np.
-// CoffeExpresStateOn
-// CoffeExpresStateOff
-// CoffeExpresStateMakeCoffe
-// CoffeExpresStateCleanUp
-// zaprezentuj zmiennosc zachowania metod expresu na przykladzie
 
-public class StateTask {
+
+// utworz interfejs Person reprezentujacy czlowieka,
+// ma on zawierac metody drink, soberup, walk, talk, setState
+// zaimplementuj obsluge nastepujacych stanow dla Person
+// - sober
+// - drunk
+// zaprezentuj zmiennosc dzialania walk and talk po zmianie stanu
+class StateTask {
     public static void main(String[] args) {
 
     }
