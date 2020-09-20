8d097992b18d3a771255654a414074e00fcc1387
Uzytkownik <uzytkownik@kursGIT.pl>
2020-09-20 12:55:08 +0200

    TemplateMethodTask

--- src/main/java/pl/sda/wzorce/behawioralne/templatemethod/zadanie/TemplateMethodTask.java
+++ src/main/java/pl/sda/wzorce/behawioralne/templatemethod/zadanie/TemplateMethodTask.java
@@ -1,6 +1,5 @@
 package pl.sda.wzorce.behawioralne.templatemethod.zadanie;
 
-
 // napisz algorytm bazowy dla tanca ktory bedzie zawierac
 // metode dance() z wywolaniem metod:
 // stepA - system.out.pr... klasniecie
@@ -12,6 +11,58 @@ package pl.sda.wzorce.behawioralne.templatemethod.zadanie;
 
 public class TemplateMethodTask {
     public static void main(String[] args) {
+        PolskiTaniec polskiTaniec = new PolskiTaniec();
+        CzeskiTaniec czeskiTaniec = new CzeskiTaniec();
 
+        polskiTaniec.dance();
+        czeskiTaniec.dance();
+    }
+}
+
+abstract class Taniec {
+
+    void dance() {
+        stepA();
+        stepB();
+        stepC();
+        stepD();
+    }
+
+    protected void stepA() {
+        System.out.println("klasniecie");
+    }
+
+    protected abstract void stepB();
+
+    protected void stepC() {
+        System.out.println("obrot");
+    }
+
+    protected abstract void stepD();
+}
+
+class PolskiTaniec extends Taniec {
+
+    @Override
+    protected void stepB() {
+        System.out.println("dwa kroki w prawo");
+    }
+
+    @Override
+    protected void stepD() {
+        System.out.println("dwa kroki w lewo");
+    }
+}
+
+class CzeskiTaniec extends Taniec {
+
+    @Override
+    protected void stepB() {
+        System.out.println("krok w tyl");
+    }
+
+    @Override
+    protected void stepD() {
+        System.out.println("dwa salta i piruet");
     }
 }
