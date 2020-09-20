2d9411372d3c9f083d4477008b8b624c0a6aa638
Uzytkownik <uzytkownik@kursGIT.pl>
2020-09-20 13:18:32 +0200

    PrototypeTask

--- src/main/java/pl/sda/wzorce/konstrukcyjne/prototype/zadanie/PrototypeTask.java
+++ src/main/java/pl/sda/wzorce/konstrukcyjne/prototype/zadanie/PrototypeTask.java
@@ -1,11 +1,36 @@
 package pl.sda.wzorce.konstrukcyjne.prototype.zadanie;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 // utworz klase Dog z polem name i type (jako enum z rasa), age
 // zaimplementuj mechanizm klonowania
 // utworz watache psow z roznymi imionami oraz wiekiem korzystajac z
 // mechanizmu klonowania
 public class PrototypeTask {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
+        Dog dog1 = new Dog("reksio", Breed.HE, 5);
+        Dog dog2 = dog1.createClone();
+        dog2.setName("winston");
+        dog2.setAge(7);
+        System.out.println(dog1.toString());
+        System.out.println(dog2.toString());
+    }
+}
+
+enum Breed {
+    SHE, HE
+}
+
+@Data
+@AllArgsConstructor
+class Dog implements Cloneable {
+    String name;
+    Breed breed;
+    int age;
+
+    Dog createClone() throws CloneNotSupportedException {
+        return (Dog) clone();
     }
 }
